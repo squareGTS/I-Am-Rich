892ec29ece3a29f2c80cda6dff3c6530b166cb7b
Maxim <68609996+squareGTS@users.noreply.github.com>
2020-11-25 10:48:24 +0300

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Maxim Bekmetov on 20.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
